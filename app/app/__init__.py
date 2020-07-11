87360aba390cff8ca83c73c9641bbadcac7bc8a7
robcreel <robcreel@gmail.com>
2020-07-11 12:16:20 -0500

    remove virtual environments from git directory

--- app/app/__init__.py
+++ app/app/__init__.py
@@ -1,7 +1,20 @@
 from flask import Flask
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
 from app import views
 from app import admin_views
 
+from .extensions import mongo
+from .main import main
+
+def create_app(config_object='app.settings'):
+    app = Flask(__name__)
+
+    app.config.from_object(config_object)
+
+    mongo.init_app(app)
+
+    app.register_blueprint(main)
+    
+    return app
